fd09158ca5a3b962db9a4f098d01e60292648433
Galvier <jonathangalvier@gmail.com>
2022-08-13 03:55:56 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,81 +1,132 @@
-import React, { useEffect, useState } from "react";
-import myEpicNft from "./utils/MyEpicNFT.json";
-import "./styles/App.css";
-import twitterLogo from "./assets/twitter-logo.svg";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react"
+import "./styles/App.css"
+import twitterLogo from "./assets/twitter-logo.svg"
+import { ethers } from "ethers"
+import myEpicNft from "./utils/MyEpicNFT.json"
 
-const TWITTER_HANDLE = "Web3dev_";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
-const TOTAL_MINT_COUNT = 50;
-const CONTRACT_ADDRESS = "0x5Af03794F506e4A6Cf24F2086e5393236760085A";
+// Constants
+const TWITTER_HANDLE = "web3dev_"
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+const OPENSEA_LINK = ""
+const TOTAL_MINT_COUNT = 50
+
+// Eu movi o endereço do contrato para cima para ficar fácil acessar
+const CONTRACT_ADDRESS = "00x5Af03794F506e4A6Cf24F2086e5393236760085A"
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
+  /*
+   * Só uma variável de estado que usamos pra armazenar nossa carteira pública. Não esqueça de importar o useState.
+   */
+  const [currentAccount, setCurrentAccount] = useState("")
+
   const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window;
+    /*
+     * Primeiro tenha certeza que temos acesso a window.ethereum
+     */
+    const { ethereum } = window
     if (!ethereum) {
-      console.log("Certifique-se que você tem a MetaMask instalada!")
-      return;
+      console.log("Certifique-se que você tem metamask instalado!")
+      return
     } else {
       console.log("Temos o objeto ethereum!", ethereum)
     }
-    const accounts = await ethereum.request({ method: "eth_accounts" });
+    /*
+     * Checa se estamos autorizados a carteira do usuário.
+     */
+    const accounts = await ethereum.request({ method: "eth_accounts" })
+    /*
+     * Usuário pode ter múltiplas carteiras autorizadas, nós podemos pegar a primeira que está lá!
+     */
     if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account:", account);
-      setCurrentAccount(account);
+      const account = accounts[0]
+      console.log("Encontrou uma conta autorizada:", account)
+      setCurrentAccount(account)
+
+      // Setup listener! Isso é para quando o usuário vem no site
+      // e já tem a carteira conectada e autorizada
+      setupEventListener()
     } else {
-      console.log("No authorized account found");
+      console.log("Nenhuma conta autorizada foi encontrada")
     }
-  };
-  /*
-   * Implemente seu método connectWallet aqui
-   */
+  }
   const connectWallet = async () => {
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
       if (!ethereum) {
-        alert("Baixe a MetaMask!");
-        return;
+        alert("Baixe o Metamask!")
+        return
       }
       /*
        * Método chique para pedir acesso a conta.
        */
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
-      });
+      })
       /*
-       * Boom! Isso deve escrever o endereço público uma vez que autorizar a MetaMask.
+       * Boom! Isso deve escrever o endereço público uma vez que autorizar o Metamask.
        */
-      console.log("Conectado", accounts[0]);
-      setCurrentAccount(accounts[0]);
+      console.log("Conectado", accounts[0])
+      setCurrentAccount(accounts[0])
+
+      // Setup listener! Para quando o usuário vem para o site
+      // e conecta a carteira pela primeira vez
+      setupEventListener()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
-  const askContractToMintNft = async () => {
-    
+  }
+
+  // Setup do listener.
+  const setupEventListener = async () => {
+    // é bem parecido com a função
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
+
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS,myEpicNft.abi,signer);
-        console.log("Vai abrir a carteira agora para pagar o gás...");
-        let nftTxn = await connectedContract.makeAnEpicNFT();
-        console.log("Mintando...espere por favor.");
-        await nftTxn.wait();
-        console.log(
-          `Mintado, veja a transação: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
-        );
+        // mesma coisa de novo
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer)
+
+        // Aqui está o tempero mágico.
+        // Isso essencialmente captura nosso evento quando o contrato lança
+        // Se você está familiar com webhooks, é bem parecido!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(
+            `Olá pessoal! Já cunhamos seu NFT. Pode ser que esteja branco agora. Demora no máximo 10 minutos para aparecer no OpenSea. Aqui está o link: <https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}>`
+          )
+        })
+
+        console.log("Setup event listener!")
       } else {
-        console.log("Objeto ethereum não existe!");
+        console.log("Objeto ethereum não existe!")
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer)
+
+        console.log("Vai abrir a carteira agora para pagar o gás...")
+        let nftTxn = await connectedContract.makeAnEpicNFT()
+        console.log("Cunhando...espere por favor.")
+        await nftTxn.wait()
+        console.log(`Cunhado, veja a transação: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`)
+      } else {
+        console.log("Objeto ethereum não existe!")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   // Métodos para Renderizar
   const renderNotConnectedContainer = () => (
@@ -83,26 +134,24 @@ const App = () => {
       Conectar Carteira
     </button>
   )
-  
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
   /*
-   * Adicionei um render condicional! Nós não queremos mostrar o Connect to Wallet se já estivermos conectados
+   * Isso roda nossa função quando a página carrega.
    */
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">Minha Coleção NFT</p>
-          <p className="sub-text">
-            Únicas. Lindas. Descubra a sua NFT hoje.
-          </p>
+          <p className="header gradient-text">Minha Coleção de NFT</p>
+          <p className="sub-text">Exclusivos! Maravilhosos! Únicos! Descubra seu NFT hoje.</p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
-              Mintar NFT
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+              Cunhar NFT
             </button>
           )}
         </div>
@@ -113,10 +162,11 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`feito com ❤️ por Jonathan através da @${TWITTER_HANDLE}`}</a>
+          >{`feito com ❤️ pela @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
-  );
-};
-export default App;
+  )
+}
+
+export default App
